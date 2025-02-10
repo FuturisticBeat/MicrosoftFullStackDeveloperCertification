781cb03ec4279699a5c72f9e8fef508eb0e049e0
Michael Maranan <michaelthommaranan@gmail.com>
2025-02-09 22:41:02 -0800

    Add course 5 StoreApp project

---
+++ BackendDevelopmentWithDotnet/StoreApp/Controllers/StoreController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    [ApiController]
+    [Route("api/store")]
+    public class StoreController : ControllerBase
+    {
+        private static List<Item> _items = new List<Item>();
+
+        [HttpGet("items")]
+        public List<Item> GetItems() => _items;
+
+        [HttpPost("items/addnew")]
+        public IActionResult AddItem(Item item)
+        {
+            Item? existingItem = _items.FirstOrDefault();
+
+            if (existingItem != null)
+            {
+                return Conflict(new { message = "Item already exists." });
+            }
+
+            _items.Add(item);
+            return CreatedAtAction(nameof(GetItems), new { id = _items.Count - 1 }, item);
+        }
+
+        [HttpPut("items/update/{id:int}")]
+        public IActionResult UpdateItem(int id, Item item)
+        {
+            if (id < 0 || id >= _items.Count)
+            {
+                return NotFound(new { message = "Item doesn't exist." });
+            }
+            
+            _items[id] = item;
+            return Ok(item);
+        }
+
+        [HttpDelete("items/delete/{id:int}")]
+        public IActionResult DeleteItem(int id)
+        {
+            if (id < 0 || id >= _items.Count)
+            {
+                return NotFound(new { message = "Item doesn't exist." });
+            }
+
+            _items.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
---
+++ BackendDevelopmentWithDotnet/StoreApp/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿namespace StoreApp.Models
+{
+    public class Item
+    {
+        public required string Name { get; set; } = string.Empty;
+        public required string Description { get; set; } = string.Empty;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Item otherItem &&
+                   Name == otherItem.Name &&
+                   Description == otherItem.Description;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Description);
+        }
+    }
+}
