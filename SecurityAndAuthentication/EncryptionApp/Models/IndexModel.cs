f15fdc6b5ba46a9482a3df26d1565df91e5b24fe
Michael Maranan <michaelthommaranan@gmail.com>
2025-02-24 23:19:48 -0800

    Add course 8 EncryptionApp

---
+++ SecurityAndAuthentication/EncryptionApp/Models/IndexModel.cs
@@ -0,0 +1,44 @@
+﻿using EncryptionApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EncryptionApp.Models
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EncryptionService _encryptionService;
+        
+        [BindProperty]
+        public IFormFile? UploadedFile { get; set; }
+        
+        public IndexModel(EncryptionService encryptionService)
+        {
+            _encryptionService = encryptionService;
+        }
+
+        public void OnPostEncrypt()
+        {
+            if (UploadedFile == null)
+            {
+                return;
+            }
+            string inputPath = Path.Combine("wwwroot", UploadedFile.FileName);
+            string encryptedPath = Path.Combine("wwwroot", $"encrypted_{UploadedFile.FileName}");
+            using FileStream stream = new(inputPath, FileMode.Create);
+            UploadedFile.CopyTo(stream);
+            _encryptionService.EncryptFile(inputPath, encryptedPath);
+        }
+
+        public void OnPostDecrypt()
+        {
+            if (UploadedFile == null)
+            {
+                return;
+            }
+            
+            string encryptedPath = Path.Combine("wwwroot", UploadedFile.FileName);
+            string decryptedPath = Path.Combine("wwwroot", $"decrypted{UploadedFile.FileName}");
+            _encryptionService.DecryptFile(encryptedPath, decryptedPath);
+        }
+    }
+}
---
+++ SecurityAndAuthentication/EncryptionApp/Services/EncryptionService.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+
+namespace EncryptionApp.Services
+{
+    public class EncryptionService
+    {
+        private readonly byte[] _key;
+        private readonly byte[] _iv;
+
+        public EncryptionService(IConfiguration configuration)
+        {
+            IConfiguration encryptionSettings = configuration.GetSection("Encryption");
+            _key = Convert.FromBase64String(encryptionSettings["Key"] ?? 
+                                            throw new InvalidOperationException("Encryption key not set."));
+            _iv = Convert.FromBase64String(encryptionSettings["IV"] ?? 
+                                            throw new InvalidOperationException("Encryption iv not set."));
+        }
+        
+        public void EncryptFile(string inputPath, string outputPath)
+        {
+            using Aes aes = Aes.Create();
+            using ICryptoTransform encryptor = aes.CreateEncryptor(_key, _iv);
+            using FileStream inputStream = new (inputPath, FileMode.Open);
+            using FileStream outputStream = new (outputPath, FileMode.Create);
+            using CryptoStream cryptoStream = new (outputStream, encryptor, CryptoStreamMode.Write);
+            inputStream.CopyTo(cryptoStream);
+        }
+
+        public void DecryptFile(string inputPath, string outputPath)
+        {
+            using Aes aes = Aes.Create();
+            using ICryptoTransform encryptor = aes.CreateDecryptor(_key, _iv);
+            using FileStream inputStream = new (inputPath, FileMode.Open);
+            using FileStream outputStream = new (outputPath, FileMode.Create);
+            using CryptoStream cryptoStream = new (outputStream, encryptor, CryptoStreamMode.Read);
+            cryptoStream.CopyTo(outputStream);
+        }
+    }
+}
